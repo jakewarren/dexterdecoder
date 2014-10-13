330df5ee4b677c8ed66b14a0fbfbea925d39871a
jakewarren <jakewarren@users.noreply.github.com>
2014-10-13 11:49:37 -0500

    Create DexterDecoder.py
    initial release

---
+++ DexterDecoder.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+
+# Description: Decodes the post data from the Dexter PoS malware
+# Author: Jake Warren
+# Reference Link: http://blog.spiderlabs.com/2012/12/the-dexter-malware-getting-your-hands-dirty.html
+
+
+from operator import xor
+import base64
+import array
+
+#fill in this variable with the posted data from the POST request
+postdata =""
+
+
+xorkey = base64.b64decode(variables['val']) 
+print "XOR Key: "+ xorkey
+print
+
+# decode all variables
+for var in variables:
+	rawdata = array.array("B",base64.b64decode(variables[var] ))
+	for i in range(len(rawdata)):
+		for char in xorkey:
+			rawdata[i] ^= ord(char)
+	variables[var] = rawdata.tostring()
+
+# print all variables
+for var in variables:
+	data = variables[var]
+
+	if var == "val": #skip the encryption key
+		continue
+	if var == "page":
+		print var +" (Mutex String):  " +variables[var] 
+	if var == "ump":
+		print var +" (Track data):  " +variables[var]
+	if var == "unm":
+		print var +" (Username):  " +variables[var]
+	if var == "cnm":
+		print var +" (Hostname):  " +variables[var] 
+	if var == "query":
+		print var +" (Victim OS):  " +variables[var]
+	if var == "spec":
+		print var +" (Processor type):  " +variables[var]
+	if var == "opt":
+		print var +" (Unknown):  " +variables[var] 
+	if var == "view":
+		print var +" (Process List):  " +variables[var]
+	if var == "var":
+		print var +" (Campaign name?):  " +variables[var]
+	
+
+
+
+
